b8ff148b78604435bb4eba8d733180e353eb3c7a
abbe <23726079+abbe@users.noreply.github.com>
2020-10-17 20:05:03 +0200

    Delete AntiOwnershipDesync.h

--- Antis/AntiOwnershipDesync.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-
-typedef int(__fastcall* oneventphoton_fn)(uintptr_t);
-extern oneventphoton_fn original_onevent_photon;
-int on_event_photon(uintptr_t event_data);
-
-void initialize_anti_ownership_desync();
